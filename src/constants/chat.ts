a367f1859730b8d02c6e5aefb4542af2df05ab6b
Alexander01998 <contact.wurstimperium@gmail.com>
2023-11-06 20:54:26 +0100

    Add cost & max tokens data for new models

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -22,9 +22,14 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
-  // 'gpt-3.5-turbo-0301',
-  // 'gpt-4-0314',
-  // 'gpt-4-32k-0314',
+  'gpt-3.5-turbo-0301',
+  'gpt-3.5-turbo-0613',
+  'gpt-3.5-turbo-16k-0613',
+  'gpt-3.5-turbo-1106',
+  'gpt-4-0314',
+  'gpt-4-0613',
+  'gpt-4-1106-preview',
+  'gpt-4-32k-0314',
 ];
 
 export const defaultModel = 'gpt-3.5-turbo';
@@ -35,9 +40,11 @@ export const modelMaxToken = {
   'gpt-3.5-turbo-0613': 4096,
   'gpt-3.5-turbo-16k': 16384,
   'gpt-3.5-turbo-16k-0613': 16384,
+  'gpt-3.5-turbo-1106': 16384,
   'gpt-4': 8192,
   'gpt-4-0314': 8192,
   'gpt-4-0613': 8192,
+  'gpt-4-1106-preview': 128000,
   'gpt-4-32k': 32768,
   'gpt-4-32k-0314': 32768,
   'gpt-4-32k-0613': 32768,
@@ -61,6 +68,10 @@ export const modelCost = {
     completion: { price: 0.004, unit: 1000 },
   },
   'gpt-3.5-turbo-16k-0613': {
+    prompt: { price: 0.001, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
+  },
+  'gpt-3.5-turbo-1106': {
     prompt: { price: 0.003, unit: 1000 },
     completion: { price: 0.004, unit: 1000 },
   },
@@ -75,6 +86,10 @@ export const modelCost = {
   'gpt-4-0613': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
+  },
+  'gpt-4-1106-preview': {
+    prompt: { price: 0.01, unit: 1000 },
+    completion: { price: 0.03, unit: 1000 },
   },
   'gpt-4-32k': {
     prompt: { price: 0.06, unit: 1000 },
