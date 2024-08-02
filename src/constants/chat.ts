f75239c7776fd40c7f7bf9336e4a39496547e584
Alexander01998 <contact.wurstimperium@gmail.com>
2024-08-02 11:54:16 +0200

    Reorder modelOptions array

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -16,19 +16,19 @@ export const _defaultSystemMessage =
   ``;
 
 export const modelOptions: ModelOptions[] = [
-  'gpt-3.5-turbo',
-  'gpt-3.5-turbo-1106',
-  'gpt-3.5-turbo-0125',
-  'gpt-3.5-turbo-16k',
-  'gpt-4',
-  'gpt-4-0613',
-  'gpt-4-1106-preview',
-  'gpt-4-0125-preview',
+  'gpt-4o',
+  'gpt-4o-2024-05-13',
   'gpt-4-turbo',
   'gpt-4-turbo-preview',
   'gpt-4-turbo-2024-04-09',
-  'gpt-4o',
-  'gpt-4o-2024-05-13',
+  'gpt-4',
+  'gpt-4-0125-preview',
+  'gpt-4-1106-preview',
+  'gpt-4-0613',
+  'gpt-3.5-turbo',
+  'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo-0125',
+  'gpt-3.5-turbo-1106',
 ];
 
 export const defaultModel = 'gpt-4o-2024-05-13';
